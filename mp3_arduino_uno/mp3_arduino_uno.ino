6a7ce077eff7baa06ff745fbcf20aab72d9a5730
yourk <2109065@upy.edu.mx>
2022-06-14 18:11:24 -0500

    Solucion de errores

--- mp3_arduino_uno/mp3_arduino_uno.ino
+++ mp3_arduino_uno/mp3_arduino_uno.ino
@@ -4,12 +4,12 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-
 #define ancho 128
 #define alto 64
-
 #define oled_reset 2
-Adafruit_SSD1306 oled(ancho,alto,&Wire,oled_reset);
+Adafruit_SSD1306 oled(ancho,alto,&Wire,oled_reset); //Todos las librerias
+ 
+//----------------------------------------------------------------------
 
 SoftwareSerial mySoftwareSerial(10, 11); // RX, TX
 DFRobotDFPlayerMini myDFPlayer;
@@ -20,23 +20,27 @@ int siguiente=5;
 int anterior=6;
 int pot=A0;
 int led=7;
-int t,play,sto,next,bef,vol,v,po,d;
-
+int t,play,sto,next,bef,vol,v,po,d; //Configuracion de pins
+//---------------------------------------------------------------------
 void setup()
 {
-  mySoftwareSerial.begin(9600);
+  setupy(); //llamar variable map
+  
+  mySoftwareSerial.begin(9600); //Configuracion de monitor
   Serial.begin(9600);
-t=1;
-pinMode(reproducir,INPUT);
-pinMode(parar,INPUT);
-pinMode(siguiente,INPUT);
-pinMode(anterior,INPUT);
   Serial.println();
   Serial.println(F("DFRobot DFPlayer Mini Demo"));
   Serial.println(F("Initializing DFPlayer ... (May take 3~5 seconds)"));
+    
+pinMode(reproducir,INPUT); //Configuracion de inputs
+pinMode(parar,INPUT);
+pinMode(siguiente,INPUT);
+pinMode(anterior,INPUT);
   
+  
+//------------------------------------------------------
 
-  if (!myDFPlayer.begin(mySoftwareSerial)) {  //Use softwareSerial to communicate with mp3.
+if (!myDFPlayer.begin(mySoftwareSerial)) {  //Use softwareSerial to communicate with mp3.
     Serial.println(F("Unable to begin:"));
     Serial.println(F("1.Please recheck the connection!"));
     
@@ -50,15 +54,18 @@ pinMode(anterior,INPUT);
     
     while(true);
   }
-  Serial.println(F("DFPlayer Mini online."));
+//--------------------------------------------
+
+Serial.println(F("DFPlayer Mini online."));
 myDFPlayer.outputDevice(DFPLAYER_DEVICE_SD);
 
   myDFPlayer.volume(10);  //Set volume value. From 0 to 30
   myDFPlayer.play(t);  //Play the first mp3
   Wire.begin();
-oled.begin(SSD1306_SWITCHCAPVCC,0x3C);
+  oled.begin(SSD1306_SWITCHCAPVCC,0x3C);
 v=0;
 d=1;
+t=1;
 }
 
 void loop()
@@ -71,14 +78,17 @@ void loop()
   po=analogRead(pot);
     v=po/34.1;
     delay(20);
-  if (vol!=v){
+    
+if (vol!=v){ //si vulmen es diferente
     vol=v;
     Serial.println (vol);
     myDFPlayer.volume(vol);
     
    
-  }
-  if (play==0){
+}
+//----------------------
+if (play==0)
+{
     delay(2);
     d++;
     if (d==1){
@@ -90,24 +100,31 @@ void loop()
       start();
     }
   }
+//-------------------------
   if (bef==0){
     delay (2);
     myDFPlayer.previous();
     before();
     t=t-1;
-    if (t==0){
+    if (t==0)
+    {
       t=1;
-    }else{
+    }
+     else{
       t=t;
     }
   }
-  if (next==0){
+//-------------
+  if (next==0)
+  {
     delay(2);
     t++;
     myDFPlayer.next();
     continous();
   }
-  if (sto==0){
+
+  if (sto==0)
+  {
     delay(2);
     myDFPlayer.pause();
     stopped();
@@ -118,115 +135,4 @@ void loop()
   }
   
   
-}
-
-void printDetail(uint8_t type, int value){
-  switch (type) {
-    case TimeOut:
-      Serial.println(F("Time Out!"));
-      break;
-    case WrongStack:
-      Serial.println(F("Stack Wrong!"));
-      break;
-    case DFPlayerCardInserted:
-      Serial.println(F("Card Inserted!"));
-      break;
-    case DFPlayerCardRemoved:
-      Serial.println(F("Card Removed!"));
-      break;
-    case DFPlayerCardOnline:
-      Serial.println(F("Card Online!"));
-      break;
-    case DFPlayerPlayFinished:
-      Serial.print(F("Number:"));
-      Serial.print(value);
-      Serial.println(F(" Play Finished!"));
-      break;
-    case DFPlayerError:
-      Serial.print(F("DFPlayerError:"));
-      switch (value) {
-        case Busy:
-          Serial.println(F("Card not found"));
-          break;
-        case Sleeping:
-          Serial.println(F("Sleeping"));
-          break;
-        case SerialWrongStack:
-          Serial.println(F("Get Wrong Stack"));
-          break;
-        case CheckSumNotMatch:
-          Serial.println(F("Check Sum Not Match"));
-          break;
-        case FileIndexOut:
-          Serial.println(F("File Index Out of Bound"));
-          break;
-        case FileMismatch:
-          Serial.println(F("Cannot Find File"));
-          break;
-        case Advertise:
-          Serial.println(F("In Advertise"));
-          break;
-        default:
-          break;
-      }
-      break;
-    default:
-      break;
-  }
-}
-void before(){
-  oled.clearDisplay();
-    oled.setCursor(10,25);
-    oled.setTextColor(WHITE);
-    oled.setTextSize(2);
-    oled.print("Before");
-    oled.display();
-    delay(6000);
-    oled.clarDisplay();
-}
-void continous(){
-  oled.clearDisplay();
-    oled.setCursor(10,25);
-    oled.setTextColor(WHITE);
-    oled.setTextSize(2);
-    oled.print("Next");
-    oled.display();
-    delay(6000);
-    oled.clearDisplay();
-}
-void stopped(){
-  oled.clearDisplay();
-    oled.setCursor(10,25);
-    oled.setTextColor(WHITE);
-    oled.setTextSize(2);
-    oled.print("Stop");
-    oled.display();
-    delay(6000);
-    oled.clearDisplay();
-}
-void start(){
-  oled.clearDisplay();
-    oled.setCursor(10,25);
-    oled.setTextColor(WHITE);
-    oled.setTextSize(2);
-    oled.print("Play");
-    oled.display();
-    delay(6000);
-    oled.clearDisplay();
-    oled.setCursor(80,50);
-    oled.setTextColor(WHITE); //White
-    oled.setTextSize(1);
-    oled.print("Playing... ");
-    oled.print(t);
-    oled.display();
-}
-void volumen(){
-  oled.clearDisplay();
-    oled.setCursor(80,25);
-    oled.setTextColor(WHITE);
-    oled.setTextSize(2);
-    oled.print(vol);
-    oled.display();
-    delay(3000);
-    oled.clearDisplay();
 }
--- mp3_arduino_uno/upy.ino
+++ mp3_arduino_uno/upy.ino
@@ -1,5 +1,20 @@
+/*How to display images on arduino with 0.96" OLED
+  subscribe for more arduino Tuorials and Projects
+  Mission Critical
+  https://www.youtube.com/channel/UCM6rbuieQBBLFsxszWA85AQ?sub_confirmation=1
+  www.minov.in
+ */
 
-void upy(){
+
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
+
+// this is the bitmap, change this variable for your specification.
 const unsigned char myBitmap [] PROGMEM = {
   0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
   0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
@@ -66,15 +81,117 @@ const unsigned char myBitmap [] PROGMEM = {
   0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
   0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
 };
-};
-void setupy(){
+
+void before(){
+  oled.clearDisplay();
+    oled.setCursor(10,25);
+    oled.setTextColor(WHITE);
+    oled.setTextSize(2);
+    oled.print("Before");
+    oled.display();
+    delay(6000);
+    oled.clearDisplay();
+}
+void continous(){
+  oled.clearDisplay();
+    oled.setCursor(10,25);
+    oled.setTextColor(WHITE);
+    oled.setTextSize(2);
+    oled.print("Next");
+    oled.display();
+    delay(6000);
+    oled.clearDisplay();
+}
+void stopped(){
+  oled.clearDisplay();
+    oled.setCursor(10,25);
+    oled.setTextColor(WHITE);
+    oled.setTextSize(2);
+    oled.print("Stop");
+    oled.display();
+    delay(6000);
+    oled.clearDisplay();
+}
+void start(){
+  oled.clearDisplay();
+    oled.setCursor(10,25);
+    oled.setTextColor(WHITE);
+    oled.setTextSize(2);
+    oled.print("Play");
+    oled.display();
+    delay(6000);
+    oled.clearDisplay();
+    display.drawBitmap(0, 0, myBitmap, 128, 64, WHITE);
+     display.display();
+}
+void volumen(){
+  oled.clearDisplay();
+    oled.setCursor(80,25);
+    oled.setTextColor(WHITE);
+    oled.setTextSize(2);
+    oled.print(vol);
+    oled.display();
+    delay(3000);
+    oled.clearDisplay();
+}
+void setupy() 
+{
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // here the 0x3c is the I2C address, check your i2c address if u have multiple devices.
   display.clearDisplay();
   delay(2000);
-}
 
-void draw();
-{
-  display.drawBitmap(0, 0, myBitmap, 128, 64, WHITE);
-  display.display();
-};
+}
+void printDetail(uint8_t type, int value){
+  switch (type) {
+    case TimeOut:
+      Serial.println(F("Time Out!"));
+      break;
+    case WrongStack:
+      Serial.println(F("Stack Wrong!"));
+      break;
+    case DFPlayerCardInserted:
+      Serial.println(F("Card Inserted!"));
+      break;
+    case DFPlayerCardRemoved:
+      Serial.println(F("Card Removed!"));
+      break;
+    case DFPlayerCardOnline:
+      Serial.println(F("Card Online!"));
+      break;
+    case DFPlayerPlayFinished:
+      Serial.print(F("Number:"));
+      Serial.print(value);
+      Serial.println(F(" Play Finished!"));
+      break;
+    case DFPlayerError:
+      Serial.print(F("DFPlayerError:"));
+      switch (value) {
+        case Busy:
+          Serial.println(F("Card not found"));
+          break;
+        case Sleeping:
+          Serial.println(F("Sleeping"));
+          break;
+        case SerialWrongStack:
+          Serial.println(F("Get Wrong Stack"));
+          break;
+        case CheckSumNotMatch:
+          Serial.println(F("Check Sum Not Match"));
+          break;
+        case FileIndexOut:
+          Serial.println(F("File Index Out of Bound"));
+          break;
+        case FileMismatch:
+          Serial.println(F("Cannot Find File"));
+          break;
+        case Advertise:
+          Serial.println(F("In Advertise"));
+          break;
+        default:
+          break;
+      }
+      break;
+    default:
+      break;
+  }
+}
